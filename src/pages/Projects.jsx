5b8b2f110fa8dc2ab93910850b9ffb11afe0fc1d
sirjoaogoncalves <joaogon85@gmail.com>
2023-04-17 15:12:14 +0100

    projects added

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -17,14 +17,25 @@ const Projects = () => {
 				<h1>Projects</h1>
 				<div className='project-cards'>
 					<div className='project-card'>
-						<img src={recipesImg} alt='Project 1' />
+						<img src={pongGame} alt='Project 1' />
+						<h3>Pong Game</h3>
+						<p>Basic Pong Game using Javascript. Responsive.</p>
+						<a href='https://sirjoaogoncalves.github.io/PongGame/' target='_blank' rel='noopener noreferrer'>
+							View Live
+						</a>
+						<a href='https://github.com/sirjoaogoncalves/PongGame' target='_blank' rel='noopener noreferrer'>
+							GitHub
+						</a>
+					</div>
+					<div className='project-card'>
+						<img src={recipesImg} alt='Project 2' />
 						<h3>Recipes App</h3>
 						<p>
 							Simple ReactJS app that allows users to search for recipes based on the ingredients they have in their pantry. This app was built with React. API used from{' '}
 							<b>
 								<i>Spoonacular</i>
 							</b>
-							. 'Not responsive'
+							.
 						</p>
 						<a href='https://sirjoaogoncalves.github.io/Recipes-Search-APP-ReactJS/' target='_blank' rel='noopener noreferrer'>
 							View Live
@@ -34,18 +45,19 @@ const Projects = () => {
 						</a>
 					</div>
 					<div className='project-card'>
-						<img src={regImg} alt='Project 2' />
-						<h3>Basic Registration Form</h3>
-						<p>Just a simple form that allows users to register for an account. This form was built with ReactJs, backend was not used, this is simply some basic code.</p>
-						<a href='https://sirjoaogoncalves.github.io/Basic-Registration-Form-using-React.js/' target='_blank' rel='noopener noreferrer'>
+						<img src={spaceEscape} alt='Project 3' />
+						<h3>Space Escape</h3>
+						<p>Just a basic game that works on and teaches Javascript logic. 'Working on responsive'</p>
+						<a href='https://sirjoaogoncalves.github.io/SpaceEscape/' target='_blank' rel='noopener noreferrer'>
 							View Live
 						</a>
-						<a href='https://github.com/sirjoaogoncalves/Basic-Registration-Form-using-React.js' target='_blank' rel='noopener noreferrer'>
+						<a href='https://github.com/sirjoaogoncalves/SpaceEscape' target='_blank' rel='noopener noreferrer'>
 							GitHub
 						</a>
 					</div>
+
 					<div className='project-card'>
-						<img src={eshopImg} alt='Project 3' />
+						<img src={eshopImg} alt='Project 4' />
 						<h3>Everything Shop</h3>
 						<p>Static E-commerce website. This was built with ReactJs, backend was not used, this is simply some basic code. 'Working on responsive'</p>
 						<a href='https://sirjoaogoncalves.github.io/EverythingShop/' target='_blank' rel='noopener noreferrer'>
@@ -56,7 +68,7 @@ const Projects = () => {
 						</a>
 					</div>
 					<div className='project-card'>
-						<img src={connectFour} alt='Project 4' />
+						<img src={connectFour} alt='Project 5' />
 						<h3>Connect Four Game</h3>
 						<p>Just a basic game that works on and teaches Javascript logic. 'Working on responsive'</p>
 						<a href='https://sirjoaogoncalves.github.io/ConnectFour/' target='_blank' rel='noopener noreferrer'>
@@ -67,24 +79,13 @@ const Projects = () => {
 						</a>
 					</div>
 					<div className='project-card'>
-						<img src={spaceEscape} alt='Project 5' />
-						<h3>Space Escape</h3>
-						<p>Just a basic game that works on and teaches Javascript logic. Game was made by me. Hope You Enjoy it. 'Working on responsive'</p>
-						<a href='https://sirjoaogoncalves.github.io/SpaceEscape/' target='_blank' rel='noopener noreferrer'>
+						<img src={regImg} alt='Project 6' />
+						<h3>Basic Registration Form</h3>
+						<p>Just a simple form that allows users to register for an account. This form was built with ReactJs, backend was not used, this is simply some basic code.</p>
+						<a href='https://sirjoaogoncalves.github.io/Basic-Registration-Form-using-React.js/' target='_blank' rel='noopener noreferrer'>
 							View Live
 						</a>
-						<a href='https://github.com/sirjoaogoncalves/SpaceEscape' target='_blank' rel='noopener noreferrer'>
-							GitHub
-						</a>
-					</div>
-					<div className='project-card'>
-						<img src={pongGame} alt='Project 6' />
-						<h3>Pong Game</h3>
-						<p>Basic Pong Game using Javascript. Responsive, but still working on the eventListeners for touch based devices.</p>
-						<a href='https://sirjoaogoncalves.github.io/PongGame/' target='_blank' rel='noopener noreferrer'>
-							View Live
-						</a>
-						<a href='https://github.com/sirjoaogoncalves/PongGame' target='_blank' rel='noopener noreferrer'>
+						<a href='https://github.com/sirjoaogoncalves/Basic-Registration-Form-using-React.js' target='_blank' rel='noopener noreferrer'>
 							GitHub
 						</a>
 					</div>
