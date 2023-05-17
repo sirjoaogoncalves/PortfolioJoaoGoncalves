36cd8e3f2817fc37da77b6bbdb264ab1738979c6
sirjoaogoncalves <joaogon85@gmail.com>
2023-05-17 13:16:55 +0100

    updated languages

--- src/pages/PLanguages.jsx
+++ src/pages/PLanguages.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { IoLogoJavascript, IoLogoHtml5, IoLogoCss3 } from 'react-icons/io';
+import { IoLogoJavascript} from 'react-icons/io';
 import { TbSql} from 'react-icons/tb';
-import { FaReact, FaLaravel, FaNodeJs, FaSass, FaGitAlt, FaNpm } from 'react-icons/fa';
+import { FaReact, FaNodeJs, FaSass, FaGitAlt, FaNpm } from 'react-icons/fa';
 import { SiMysql, SiPhp, SiPython, SiMongodb } from 'react-icons/si';
 import './PLanguages.css';
 import Navbar from '../components/Navbar';
 import { FcLinux } from 'react-icons/fc';
-import { DiVim } from 'react-icons/di';
-import {SiRubyonrails} from 'react-icons/si';
+import {TbBrandCpp} from 'react-icons/tb';
+import {TbBrandCSharp} from 'react-icons/tb';
+
 
 const PLanguages = () => {
 	return (
@@ -23,12 +24,8 @@ const PLanguages = () => {
 						<p className='ll-text'>JavaScript</p>
 					</div>
 					<div className='ll-item'>
-						<IoLogoHtml5 className='ll-icon' color='#ff4c05' />
-						<p className='ll-text'>HTML</p>
-					</div>
-					<div className='ll-item'>
-						<IoLogoCss3 className='ll-icon' color='#1a53ce' />
-						<p className='ll-text'>CSS</p>
+						<FaSass className='ll-icon' color=' #8f246b' />
+						<p className='ll-text'>SASS</p>
 					</div>
 					<div className='ll-item'>
 						<FaReact className='ll-icon' color='cyan' />
@@ -51,33 +48,29 @@ const PLanguages = () => {
 						<FcLinux className='ll-icon' color=' #ff3300' />
 						<p className='ll-text'>Linux</p>
 					</div>
-					<div className='ll-item'>
-						<DiVim className='ll-icon' color=' #9ae96c' />
-						<p className='ll-text'>Vim</p>
-					</div>
 				</div>
 				<div className='ll-texts'>
 					<h2 className='ll-title'>Learning</h2>
 				</div>
 				<div className='ll-list'>
 					<div className='ll-item'>
-						<SiRubyonrails className='ll-icon' color='#e70202' />
-						<p className='ll-text'>Ruby on Rails</p>
+						<TbBrandCpp className='ll-icon' color='#1d7ae6' />
+						<p className='ll-text'>C++</p>
+					</div>
+					<div className='ll-item'>
+						<TbBrandCSharp className='ll-icon' color='#c51de6' />
+						<p className='ll-text'>C#</p>
 					</div>
 					<div className='ll-item'>
 						<SiPhp className='ll-icon' color='#9999ff' />
 						<p className='ll-text'>PHP</p>
 					</div>
 					<div className='ll-item'>
-						<FaLaravel className='ll-icon' color='#ff1a1a' />
-						<p className='ll-text'>Laravel</p>
-					</div>
-					<div className='ll-item'>
 						<SiPython className='ll-icon' color=' #3366ff' />
 						<p className='ll-text'>Python</p>
 					</div>
 					<div className='ll-item'>
-						<TbSql className='ll-icon' color='purple' />
+						<TbSql className='ll-icon' color='#009991' />
 						<p className='ll-text'>SQL</p>
 					</div>
 					<div className='ll-item'>
@@ -87,10 +80,6 @@ const PLanguages = () => {
 					<div className='ll-item'>
 						<FaNodeJs className='ll-icon' color=' #009933' />
 						<p className='ll-text'>NodeJS</p>
-					</div>
-					<div className='ll-item'>
-						<FaSass className='ll-icon' color=' #8f246b' />
-						<p className='ll-text'>SASS</p>
 					</div>
 				</div>
 			</div>
